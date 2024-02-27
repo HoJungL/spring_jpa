6576f3b77e2ef95d8411b99bac0c980a72a36a07
HoJungL <ghaud5684@naver.com>
2024-02-27 21:41:04 +0900

    --springboot와 jpa!!
    --h2 db 만들때, 우선 ~/filename을 만든뒤에, tcp:localhost:/~/filename 을 써야함~!
    
    --junit 5일떄는 runwith가 아니라 extendWith임... 고쳐야함.
    assert도  import static org.junit.jupiter.api.Assertions.*;로 바뀜!!!!
    --JUnit5에서는 @Test(expect)지원안합니다. 대신, Assertions.assertThrows로 설정가능함!
    
    -- Xto One 관계에서는 반드시 fetch = LAZY(지연로딩)으로 할것. EAGER로 하면 효율성 저하됨.
    
    --MemberRepository : 회원 리퍼지토리
    
    --MemberService : 회원 서비스
    
    ** 변경 감지와 merge

--- jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
@@ -1,10 +1,17 @@
 package jpabook.jpashop.Controller;
 
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +24,31 @@ public class MemberController {
         // memberForm이라는 빈 껍데기를 들고 가는거임
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    //회원 등록
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    // 회원 목록 조회
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
