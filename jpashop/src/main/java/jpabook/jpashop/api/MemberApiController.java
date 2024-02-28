6979254bc70fd27e6a4a86ea73584b4acbb06a6c
HoJungL <ghaud5684@naver.com>
2024-02-28 15:05:40 +0900

    --springboot와 jpa!!
    --h2 db 만들때, 우선 ~/filename을 만든뒤에, tcp:localhost:/~/filename 을 써야함~!
    
    --junit 5일떄는 runwith가 아니라 extendWith임... 고쳐야함.
    assert도  import static org.junit.jupiter.api.Assertions.*;로 바뀜!!!!
    --JUnit5에서는 @Test(expect)지원안합니다. 대신, Assertions.assertThrows로 설정가능함!
    
    -- Xto One 관계에서는 반드시 fetch = LAZY(지연로딩)으로 할것. EAGER로 하면 효율성 저하됨.
    
    --MemberRepository : 회원 리퍼지토리
    
    --MemberService : 회원 서비스
    
    ** 변경 감지와 merge (ItemService.java 확인)
    
    변경 감지 : 영속성 컨텍스트에서 entity를 다시 조회한 후에, 데이터를 수정하는 방법
    순서 : 트랜잭션 안 엔티티 조회 -> 트랜잭션 커밋 시점에 변경 감지 -> update SQL 실시
    
    merge: 모든 데이터를 바꿔버리는거임. 준영속 상태의 entity를 영속 상태로 변경할때 사용!
    순서 : 머지 -> 1차 캐시 entity 찾기 (없으면 DB 조회) -> set한걸 다 밀어 넣어버림(값 채워넣음) -> 반환
    
    근데.. 머지는 잘못하면 null로 다 바꿔버리는 경우가 생길 가능성도 있음(field가 빠져있을 경우) 다날라감.. 실무에서 큰일나요..
    
    /// JPA 활용 2
    -- Api의 entity에서 외부 노출은 위험함. api를 만들때는 entity를 파라미터로 받지말것. dto만드는게 나중을 위해서 훨씬 안전함! 제발 제발 제발!!!
    
    --- api/v1/members의 문제점
    1. entitiy에 프레젠테이션 계층을 위한 로직이 추가됨
    2. entity의 값이 노출됨
    3. 응답 스펙을 맞추기 위해 추가 로직이 생김(JsonIgnore등)
    4. 프레젠테이션 응답 로직을 담기 어려움
    5. entity가 변경되면 api 스펙 변함
    6. 컬렉션을 직접 반환하면 추후 api 스펙을 변경하기 어려움 -> 이는 별도의 result 클래스 생성으로 해결해야함.

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,12 +3,14 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // controller, ResponseBody가 둘다 담겨있는 컨트롤러
 @RequiredArgsConstructor
@@ -16,6 +18,32 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList()); // 이거 안하면 리스트로 확나가니까 유연성 떨어짐.
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result <T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
     //등록
     // 장점 : Class를 하나더 안 만들어도 됨.
     @PostMapping("/api/v1/members")
@@ -33,6 +61,31 @@ public class MemberApiController {
         Long id = memberService.join(member); // request는 안들어감!
         return new CreateMemberResponse(id);
     }
+
+    // 회원 수정 API
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMember2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        //변경감지임. update 확인해보깅
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest {
--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +20,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore // 이거하면 DB에서 무시됨.
     @OneToMany(mappedBy = "member") //mappedBy에는 변수이름!!
     private List<Order> orders = new ArrayList<>();
 }
