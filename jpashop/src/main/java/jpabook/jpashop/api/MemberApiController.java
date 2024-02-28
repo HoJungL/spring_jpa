53529934d2f5999dab4eadebe47f05b4b4558085
HoJungL <ghaud5684@naver.com>
2024-02-28 14:10:07 +0900

    --springboot와 jpa!!
    --h2 db 만들때, 우선 ~/filename을 만든뒤에, tcp:localhost:/~/filename 을 써야함~!
    
    --junit 5일떄는 runwith가 아니라 extendWith임... 고쳐야함.
    assert도  import static org.junit.jupiter.api.Assertions.*;로 바뀜!!!!
    --JUnit5에서는 @Test(expect)지원안합니다. 대신, Assertions.assertThrows로 설정가능함!
    
    -- Xto One 관계에서는 반드시 fetch = LAZY(지연로딩)으로 할것. EAGER로 하면 효율성 저하됨.
    
    --MemberRepository : 회원 리퍼지토리
    
    --MemberService : 회원 서비스
    
    ** 변경 감지와 merge (ItemService.java 확인)
    
    변경 감지 : 영속성 컨텍스트에서 entity를 다시 조회한 후에, 데이터를 수정하는 방법
    순서 : 트랜잭션 안 엔티티 조회 -> 트랜잭션 커밋 시점에 변경 감지 -> update SQL 실시
    
    merge: 모든 데이터를 바꿔버리는거임. 준영속 상태의 entity를 영속 상태로 변경할때 사용!
    순서 : 머지 -> 1차 캐시 entity 찾기 (없으면 DB 조회) -> set한걸 다 밀어 넣어버림(값 채워넣음) -> 반환
    
    근데.. 머지는 잘못하면 null로 다 바꿔버리는 경우가 생길 가능성도 있음(field가 빠져있을 경우) 다날라감.. 실무에서 큰일나요..
    
    /// JPA 활용 2
    -- Api의 entity에서 외부 노출은 위험함. api를 만들때는 entity를 파라미터로 받지말것. dto만드는게 나중을 위해서 훨씬 안전함!

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // controller, ResponseBody가 둘다 담겨있는 컨트롤러
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    //등록
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        //생성자
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
